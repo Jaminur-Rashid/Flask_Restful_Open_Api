c91f193085a0223e74745dc5e327e55e193538dc
Jaminur-Rashid <jaminurrashid21@gmail.com>
2021-12-13 00:25:10 +0600

    sort data by number od bedrooms added

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+#################################################################
+# This is Restful Open Api using flask and Jwt                   #
+# Which can search data from database using multiple properties  #
+# Author : Jaminur Rashid                                        #
+# Date   : 9-12-2021                                             #
+# #################################################################
 from flask import Flask, jsonify, request, make_response, redirect
 import jwt
 import datetime
@@ -9,9 +15,8 @@ import operator
 
 token_id = ''
 app = Flask(__name__)
-
+# Configuring Swagger
 app.config['SECRET_KEY'] = 'weengineers'
-#### Swagger path initialized ###
 SWAGGER_URL = '/swagger'
 API_URL = '/static/swagger.json'
 SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
@@ -25,10 +30,7 @@ SWAGGERUI_BLUEPRINT = get_swaggerui_blueprint(
 app.register_blueprint(SWAGGERUI_BLUEPRINT, url_prefix=SWAGGER_URL)
 
 
-### end swagger specific ###
-
 # Token Decorator
-
 
 def token_required(f):
     @wraps(f)
@@ -41,7 +43,7 @@ def token_required(f):
         try:
             data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
         except:
-            return jsonify({'message': 'Token is invalid!'}), 403
+            return jsonify({'message': 'Token is not valid'}), 403
 
         return f(*args, **kwargs)
 
@@ -56,7 +58,7 @@ def index():
 # Unprotected Route and function
 @app.route('/unprotected')
 def unprotected():
-    return jsonify({'message': 'Anyone can view this.'})
+    return jsonify({'message': 'This a public route'})
 
 
 @app.route('/house', methods=['GET'])
@@ -69,58 +71,61 @@ def house():
     location = request.args.get('location')
 
     condo = []
-
+    # Connect with vrboData database which contains the scrapped data
     db_connector = mysql.connector.connect(
         host="localhost",
         user="root",
         password="",
         database="vrboDatabase"
     )
-    check = False
-    query = "SELECT * FROM vrbo_hotel_info_table WHERE"
-    check= "SELECT * FROM vrbo_hotel_info_table WHERE"
+    data_fetch_query = "SELECT * FROM vrbo_hotel_info_table WHERE"
+    check = "SELECT * FROM vrbo_hotel_info_table WHERE"
     cursor = db_connector.cursor()
+    data_container = []
     if title != None:
-        if query is check:
-            query += " Hotel_Name LIKE" + f"'%{title}%'"
+        if data_fetch_query is check:
+            data_fetch_query += " Hotel_Name LIKE" + f"'%{title}%'"
         else:
-            query += " AND Hotel_Name LIKE" + f"'%{title}%'"
+            data_fetch_query += " AND Hotel_Name LIKE" + f"'%{title}%'"
 
     if bedroom != None:
-        if query is check:
-            query += " Bedroom >="+bedroom+""
+        if data_fetch_query is check:
+            data_fetch_query += " Bedroom >="+bedroom+""
         else:
-            query += " AND Bedroom >="+bedroom+""
+            data_fetch_query += " AND Bedroom >="+bedroom+""
 
     if bathroom != None:
-            if query is check:
-                query += " Bathroom >="+bathroom+""
+            if data_fetch_query is check:
+                data_fetch_query += " Bathroom >="+bathroom+""
             else:
-                query += " AND Bathroom >="+bathroom+""
+                data_fetch_query += " AND Bathroom >="+bathroom+""
 
     if sleeps != None:
-        if query == check:
-            query += " Sleeping >="+sleeps+""
+        if data_fetch_query == check:
+            data_fetch_query += " Sleeping >="+sleeps+""
         else:
-            query += " AND  Sleeping >="+sleeps+""
+            data_fetch_query += " AND  Sleeping >="+sleeps+""
 
     if price != None:
-        if query == check:
-            query += " Price LIKE" + f"'%{price}%'"
+        if data_fetch_query == check:
+            data_fetch_query += " Price LIKE" + f"'%{price}%'"
         else:
-            query += " Price LIKE" + f"'%{price}%'"
+            data_fetch_query += " Price LIKE" + f"'%{price}%'"
 
     if location != None:
-        if query is check:
-            query += " Location LIKE" + f"'%{location}%'"
+        if data_fetch_query is check:
+            data_fetch_query += " Location LIKE" + f"'%{location}%'"
         else:
-            query += " AND Location LIKE" + f"'%{location}%'"
-    print(query)
-    cursor.execute(query)
+            data_fetch_query += " AND Location LIKE" + f"'%{location}%'"
+    #data_fetch_query += " ORDER BY Sleeping DESC"
+    print(data_fetch_query)
+    # Execute select query to fetch data
+    cursor.execute(data_fetch_query)
     results = cursor.fetchall()
     print(results)
+    # extract the fetched data and format it
     for result in results:
-        data = {
+        hotel_data = {
             "Location": result[1],
             "Hotel Name": result[2],
             "Sleeps": result[3],
@@ -128,19 +133,26 @@ def house():
             "Bathrooms": result[5],
             "Price": result[6],
         }
-        condo.append(data)
-    condo.sort(key=operator.itemgetter('Bedrooms'))
-    houseJson = json.dumps(condo, indent=4)
-    print(condo)
-    print(houseJson)
-    print(query)
-    return houseJson
+        data_container.append(hotel_data)
+    # Sort List of dictionaries by key value
+    # Sort hotel data by num of Slepping room in Ascending order
+    # Sort is done by using string comparison
+    # as our database column name was varchar type
+    data_container.sort(key=operator.itemgetter('Bedrooms'))
+    #d = sorted(data_container, key=lambda tmp: tmp['Sleeps'])
+    # format data into json
+    hotel_data_json = json.dumps(data_container, indent=4)
+    #print(condo)
+    print(hotel_data_json)
+    print(data_fetch_query)
+    return hotel_data_json
 
 
-# Protected Route and function
-@app.route('/protected')
-#@token_required
-def protected():
+# Redirect to swagger ui page to test api
+# after login using jwt token
+@app.route('/swaggerPage')
+@token_required
+def swagger():
     # return jsonify({'message': 'Only available to people with valid tokens.'})
 
     return redirect("http://127.0.0.1:5000/swagger", code=302)
